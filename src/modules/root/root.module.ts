1ee67e61b7578021ca3255786cde1d170fbf7845
yeosiowvic <victoryeo1974@gmail.com>
2022-03-29 16:03:41 +0800

    remove TypeOrmModule UserSettingEntity from Root module

--- src/modules/root/root.module.ts
+++ src/modules/root/root.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RootService } from './root.service';
 import { RootController } from './root.controller';
 import { AuthModule } from '../auth/auth.module';
-import { UserSettingEntity } from '../../entities/usersetting.entity'; 
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([UserSettingEntity])],
+  imports: [AuthModule],
   controllers: [RootController],
   providers: [RootService],
   exports: [RootService],
--- src/modules/root/root.service.ts
+++ src/modules/root/root.service.ts
@@ -1,15 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, SelectQueryBuilder } from 'typeorm';
-import { UserSettingEntity } from '../../entities/usersetting.entity';
-import { UserSettingResourceModel } from '../../dtos/userSettingResourceModel';
-import { toUserDto } from '../../common/mapper';
+
 
 @Injectable()
 export class RootService {
   constructor(
-    @InjectRepository(UserSettingEntity)
-    private userSettingRepository: Repository<UserSettingEntity>
   ) {}
 
 
